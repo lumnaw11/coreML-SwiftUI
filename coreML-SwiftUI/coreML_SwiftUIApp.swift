c1fd198fc3262a1f9b187f9fd76ff60af0bd19e1
Lum Naw <lumnaw07@gmail.com>
2023-02-06 14:31:35 +0900

    Initial Commit

---
+++ coreML-SwiftUI/coreML_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  coreML_SwiftUIApp.swift
+//  coreML-SwiftUI
+//
+//  Created by cmStudent on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct coreML_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
